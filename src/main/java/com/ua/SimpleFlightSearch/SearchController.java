3d2b8217727be6cc2c1b5c3c199a71badf297809
eason <yul196@pitt.edu>
2019-06-28 21:13:41 -0400

    initial commit

--- src/main/java/com/ua/SimpleFlightSearch/SearchController.java
+++ src/main/java/com/ua/SimpleFlightSearch/SearchController.java
@@ -5,9 +5,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class SearchController {
 
